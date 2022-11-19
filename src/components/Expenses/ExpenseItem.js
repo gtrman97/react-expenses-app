dc0c8196b6930495c087eca85ea0c5e775d45dde
Andrew Killian <ack97@txstate.edu>
2022-11-19 15:16:34 -0600

    deleted button

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -13,7 +13,6 @@ const ExpenseItem = (props) => {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button className='change-button' >Change Title</button>
     </Card>
     </li>
   );
